d897a7e5b4cde6a7117849bc1642fe5daa5f62ab
Naman105 <goelnaman366@gmail.com>
2026-09-01 23:28:00 +0530

    changes

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -10,7 +10,7 @@ const Dashboard = () => {
   const [showUploadResume, setShowUploadResume] = useState(false)
   const [title, setTitle] = useState('')
   const [resume, setResume] = useState(null)
-  const [editResumeId, setEditResumeId] = useState(null)
+  const [editResumeId, setEditResumeId] = useState('')
 
   const loadAllResumes = async () => {
     setAllResumes(dummyResumeData)
@@ -59,7 +59,7 @@ const Dashboard = () => {
 
       {showCreateResume && (
         <form className='fixed inset-0 bg-black/70 backdrop-blur bg-opacity-50 z-10 flex items-center justify-center'>
-          <div className=''>
+          <div className='relative bg-slate-50 border shadow-md rounded-lg w-full max-w-sm p-6'>
             <h2 className='text-xl font-bold mb-4'>Create a Resume</h2>
             <input type="text" placeholder="Enter Resume Title" className='w-full px-4 py-2 mb-4 focus:border-green-600 ring-green-600' required />
 
